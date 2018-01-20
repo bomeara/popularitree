425a93949aaf9984daa7af3f29ea027380121b98
Brian O'Meara <omeara.brian@gmail.com>
2018-01-20 09:25:37 -0700

    updated help

--- R/popularitree.R
+++ R/popularitree.R
@@ -8,6 +8,15 @@
 #' @param pd_weighting How much to weight the pd vs the popularity
 #' @param nrep How many replicates to use
 #' @return A phylo object pruned to the number of taxa
+#' @examples
+#' big.phy <- rotl::get_study("ot_308")[[1]]
+#' big.phy <- geiger::drop.random(big.phy, ape::Ntip(phy) - 20)
+#' measures <- tree_compute_measures(big.phy)
+#' small.phy <- popularitree(phy=big.phy, ntax=5, measures, nrep=100)
+#' par(mfcol=c(1,3))
+#' plot(big.phy)
+#' plot(small.phy)
+#' plot(tree_convert_to_common(small.phy))
 #' @export
 popularitree <- function(phy, ntax, measures, pd_weighting=.5, nrep=10000) {
   tree.scaling <- (ntax/ape::Ntip(phy))/sum(phy$edge.length) # want the measures and the tree brlen to be on the same scale after sampling
@@ -49,10 +58,14 @@ generate_sample <- function(taxon_scores, ntax, phy, pd_weighting, min.prob=0.01
 #' Get a sum of hits of a species relative to a standard
 #' @param species The focal species to look at search interest over
 #' @param standard Since Google Trends always compares popularity, give a standard species to use
+#' @param verbose If TRUE, chat about what species is being examined
 #' @return The sum of relative hits over the time period
 #' @export
-species_gtrends_popularity <- function(species, standard="Myrmeocystus mexicanus") {
-  result <- gtrendsR::gtrends(c(species, standard), time="2017-01-01 2017-12-31")$interest_over_time
+species_gtrends_popularity <- function(species, standard="Myrmeocystus mexicanus", verbose=TRUE) {
+  if(verbose) {
+    print(paste0("Retrieving trends data for taxon '", species, "'"))
+  }
+  result <- gtrendsR::gtrends(gsub("_", " ",c(species, standard)), time="2017-01-01 2017-12-31")$interest_over_time
   result <- subset(result, result$keyword==species)
   return(sum(result$hits))
 }
@@ -66,6 +79,54 @@ species_gtrends_popularity <- function(species, standard="Myrmeocystus mexicanus
 #'
 #' @export
 species_ncbi_common_exists <- function(species) {
-  returned.name <- taxize::sci2comm(scinames=species, db='ncbi')[[1]]
-  return(length(nchar(returned.name[[1]])))
+  returned.name <- NULL
+  has_common <- 0
+  try(returned.name <- taxize::sci2comm(scinames=species, db='ncbi'))
+  if(!is.null(returned.name)) {
+    has_common <- length(nchar(returned.name[[1]]))
+  }
+  return(has_common)
+}
+
+#' Return common name if known, otherwise return scientific name
+#' @param species Species name
+#' @return Common name (ideally) or species name
+#' @export
+species_get_common <- function(species) {
+  returned.name <- NULL
+  has_common <- 0
+  try(returned.name <- taxize::sci2comm(scinames=species, db='ncbi'))
+  if(!is.null(returned.name)) {
+    has_common <- length(nchar(returned.name[[1]]))
+  }
+  final_name <- species
+  if(has_common==1) {
+    final_name <- unname(returned.name[[1]])
+  }
+  return(final_name)
+}
+
+#' Compute popularity measures
+#' @param phy An input phylogeny
+#' @return data.frame of measures
+#' @export
+tree_compute_measures <- function(phy) {
+  taxa <- phy$tip.label
+  common <- sapply(taxa, species_ncbi_common_exists)
+  common <- common/max(1,sum(common)) #to normalize the different measures
+  trends <- sapply(taxa, species_gtrends_popularity)
+  trends <- trends/max(1,sum(trends))  #to normalize the different measures
+  result <- data.frame(common=common, trends=trends)
+  rownames(result) <- taxa
+  return(result)
+}
+
+#' Convert tips to common names if possible
+#' @param phy A phylogeny
+#' @return Phylogeny with names changed
+#' @export
+tree_convert_to_common <- function(phy) {
+  phy$tip.label <- gsub(" ", "_",sapply(phy$tip.label,species_get_common))
+  return(phy)
+
 }
